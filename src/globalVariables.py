715c753430d273136da79e5580ab1d98ab1f9f60
Aravind Sri Sai Kisore <62432516+aravindcmd@users.noreply.github.com>
2023-10-04 20:41:15 +0200

    Update globalVariables.py
    added rules

--- src/globalVariables.py
+++ src/globalVariables.py
@@ -73,4 +73,39 @@ startTime = None
 variable1 = ""
 variable2 = ""
 
+
+rules = str('+===================================================================+===| +\n'
+	      '| RULES OF THE GAME :                                               |\n'
+	      '| At the beginning, every playes has 9 pieces. The goal of the game |\n'
+	      '| is to leave your opponent with only two pieces or leave him       |\n'
+	      '| without any possible moves. Table has 24 fields and it is possible|\n'
+	      '| to move only to adjacent fields, if they are free. If one of the  |\n'
+	      '| players make mill (three pieces in a row) then he can remove one  |\n'
+	      '| opponent\'s piece by choosing one of the fields where opponent     |\n'
+	      '| placed his piece. NOTE: it\'s not allowed to remove opponent piece |\n'
+	      '| if it\'s already in a MILL, unless his all pieces are in MILLS,    |\n'
+	      '| then you can remove any piece you want. You can remove piece in   |\n'
+	      '| any phase of the game.                                            |\n'
+	      '|                                                                   |\n'
+	      '| PHASES OF THE GAME:                                               |\n'
+	      '| 1. PHASE (INIT) -> in this phase both playes are intermittently   |\n                                                   |\n'
+	      '| placing pieces on free fields ( 0 - 23 on the table). When player | \n'
+	      '| run out of pieces to place, then we are moving to the next stage  |\n'
+	      '| of the game.                                                      |\n'
+	      '| 2. PHASE (MOVE) -> in this phase players can move their pieces to |\n'
+	      '| adjacent free fields. First, they input the piece they want to    |\n'
+	      '| move, and then the field where they want to move that piece.      |\n'
+	      '| 3. PHASE (FLY) -> if the player remained with only 3 pieces,      |\n'
+	      '| then he has additional opportunity to move his pieces to ANY free |\n'
+	      '| field on the table, filed doesn\'t has to be adjecent and jumping  |\n'
+	      '| over pieces is allowed.                                           |\n'
+	      '|                                                                   |\n'
+	      '| When one of the players remained with only 2 pieces, or the player|\n'
+	      '| can\'t perform any move because every adjacent field is taken, the |\n'
+	      '| game is over. Right now, your enemy is computer, can you beat him?|\n'
+	      '| You can choose if you want to play first. Yours pieces are marked |\n'
+	      '| as W (white) and computers pieces are marked as B (black).        |\n'
+	      '|                         GOOD LUCK!                                |\n'
+	      '+-------------------------------------------------------------------+\n')
+
 # ALLPLACED = False
